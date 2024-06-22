be6a28faf887e8546334e4d447bd5eda856f52ff
Antoine Bousquet antoinebousquet8@gmail.com <antoinebousquet8@gmail.com>
2024-06-22 18:00:52 +0200

    test langchain import

--- populate_database.py
+++ populate_database.py
@@ -5,6 +5,7 @@ from langchain.document_loaders.pdf import PyPDFDirectoryLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.schema.document import Document
 from langchain.vectorstores.chroma import Chroma
+from langchain_community.vectorstores import Chroma
 from get_embedding_function import get_embedding_function
 
 CHROMA_PATH = "chroma"
